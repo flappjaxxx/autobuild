321951dcd49fdaff6ffcb275f6bc6db6f5876b77
flappjaxxx <flappjaxxx@gmail.com>
2012-09-26 08:35:10 -0400

    add missing files

---
+++ repackage.sh
@@ -0,0 +1,67 @@
+START=$(date +%s)
+clear
+echo "Building The Final AOSPxXx Package"
+sleep 5
+clear
+rm -rf autobuild/cm-AOSPxXx-*.zip
+echo "Copying unfinished ROM"
+sleep 3
+cp -v out/target/product/quincyatt/cm-AOSPxXx-*.zip autobuild
+cd autobuild
+sleep 1
+clear
+echo "Removing Old Files"
+sleep 3
+rm -rfv WORKING_AOSPxXx
+rm -fv AOSPxXx-*.zip
+sleep 1
+clear
+echo "Inflating Archive"
+sleep 3
+unzip cm-AOSPxXx-*.zip -d WORKING_AOSPxXx
+sleep 1
+clear
+echo "Copying GApps"
+sleep 3
+cp -Rv gapps/system WORKING_AOSPxXx
+sleep 1
+clear
+echo "Copying Installer"
+sleep 3
+cp -Rv META-INF WORKING_AOSPxXx
+sleep 1
+clear
+echo "Copying User Apps"
+sleep 3
+cp -Rv data WORKING_AOSPxXx
+sleep 1
+clear
+echo "Removing Unneeded Apps"
+sleep 3
+rm -f WORKING_AOSPxXx/system/app/Provision.apk
+rm -f WORKING_AOSPxXx/system/app/QuickSearchBox.apk
+sed -i 's/cm_quincyatt/aospxxx_quincyatt/g' WORKING_AOSPxXx/system/build.prop
+sleep 1
+clear
+echo "Zipaligning"
+sleep 3
+tools/do_zipalign
+sleep 1
+clear
+echo "Zipping Final Package"
+sleep 3
+cd WORKING_AOSPxXx
+zip -r AOSPxXx-v2.1B7.zip *
+mv -v AOSPxXx-v2.1B7.zip ../
+cd ..
+rm -rfv autobuild/cm-AOSPxXx-*.zip
+sleep 1
+END=$(date +%s)
+ELAPSED=$((END - START))
+E_MIN=$((ELAPSED / 60))
+E_SEC=$((ELAPSED - E_MIN * 60))
+printf "Elapsed: "
+[ $E_MIN != 0 ] && printf "%d min(s) " $E_MIN
+printf "%d sec(s)\n" $E_SEC
+echo "Finished."
+echo "Final Package now in autobuild directory"
