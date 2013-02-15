6e2b06937a8a24eaa2508cd2469c3ca3dbfcbf77
flappjaxxx <flappjaxxx@gmail.com>
2013-02-15 17:56:50 -0500

    Added in repo sync command Also added quincyatt version 2 for no ccache building

--- quincyatt2.sh
+++ quincyatt2.sh
@@ -2,9 +2,6 @@ START=$(date +%s)
 clear
 make clobber
 repo sync
-export USE_CCACHE=1
-export CCACHE_DIR=/$HOME/.ccache
-prebuilts/misc/linux-x86/ccache/ccache -M 40G
 . build/envsetup.sh
 vendor/cm/get-prebuilts
 brunch cm_quincyatt-userdebug
