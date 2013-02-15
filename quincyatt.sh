205c2dc151f8ac2eb54d042c381c60c780494b12
flappjaxxx <flappjaxxx@gmail.com>
2013-02-15 17:29:26 -0500

    Initial cm-10.1 commit

---
+++ quincyatt.sh
@@ -0,0 +1,17 @@
+START=$(date +%s)
+clear
+make clobber
+export USE_CCACHE=1
+export CCACHE_DIR=/$HOME/.ccache
+prebuilts/misc/linux-x86/ccache/ccache -M 40G
+. build/envsetup.sh
+vendor/cm/get-prebuilts
+brunch cm_quincyatt-userdebug
+END=$(date +%s)
+ELAPSED=$((END - START))
+E_MIN=$((ELAPSED / 60))
+E_SEC=$((ELAPSED - E_MIN * 60))
+printf "Elapsed: "
+[ $E_MIN != 0 ] && printf "%d min(s) " $E_MIN
+printf "%d sec(s)\n" $E_SEC
+echo "Finished."
