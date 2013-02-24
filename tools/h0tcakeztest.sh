40a7379406d2763ba2793b196e443db638cdda21
flappjaxxx <flappjaxxx@gmail.com>
2013-02-24 11:33:25 -0500

    Revert "another fix"
    This reverts commit 3062cf344f4f6449697ee6ad20a78313a18d468f.

--- tools/h0tcakeztest.sh
+++ tools/h0tcakeztest.sh
@@ -1,3 +1,4 @@
+START=$(date +%s)
 clear
 make clobber
 cp -f autobuild/manifests/test_local_manifest.xml .repo/local_manifest.xml
@@ -13,4 +14,11 @@ cd autobuild/h0tcakez-Test
 zip -r h0tcakez-JB-Kernel-$(date -u +%Y%m%d)-Test.zip *
 mv h0tcakez-JB-Kernel-$(date -u +%Y%m%d)-Test.zip ../h0tcakez-JB-Kernel-$(date -u +%Y%m%d)-Test.zip
 echo "h0tcakez-JB-Kernel-$(date -u +%Y%m%d)-Test.zip now in autobuild directory"
+END=$(date +%s)
+ELAPSED=$((END - START))
+E_MIN=$((ELAPSED / 60))
+E_SEC=$((ELAPSED - E_MIN * 60))
+printf "Elapsed: "
+[ $E_MIN != 0 ] && printf "%d min(s) " $E_MIN
+printf "%d sec(s)\n" $E_SEC
 echo "Finished."
